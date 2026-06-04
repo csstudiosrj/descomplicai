31510556eafac400c7bcbc636e820d6110627b63
cscomrj02 <cscomrj02@gmail.com>
2026-06-04 12:54:43 +0000

    implementando logo e id visual

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,4 @@
-// HTML base com VLibras integrado
+// HTML base com Google Fonts + VLibras integrado
 // Dependências diretas: next/document
 
 import { Html, Head, Main, NextScript } from 'next/document';
@@ -7,6 +7,12 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500&family=Space+Mono:ital,wght@1,400&display=swap"
+          rel="stylesheet"
+        />
         <script src="https://vlibras.gov.br/app/vlibras-plugin.js" async />
       </Head>
       <body>
