651740296dd7abe5dbf0557e279a13bf0d92c9b6
cscomrj02 <cscomrj02@gmail.com>
2026-07-09 14:37:11 +0000

    mudando o next pra testar o MP

--- next.config.js
+++ next.config.js
@@ -15,6 +15,29 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['framer-motion', '@react-pdf/renderer'],
   },
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://sdk.mercadopago.com https://*.mercadopago.com https://*.mercadolibre.com https://*.google.com https://*.googleapis.com https://*.gstatic.com",
+              "style-src 'self' 'unsafe-inline' https://*.mercadopago.com https://fonts.googleapis.com",
+              "img-src 'self' data: blob: https://*.mercadopago.com https://*.mercadolibre.com https://images.unsplash.com https://lh3.googleusercontent.com https://**.supabase.co",
+              "connect-src 'self' https://*.mercadopago.com https://*.mercadolibre.com https://api.mercadopago.com https://**.supabase.co https://*.google.com",
+              "frame-src 'self' https://*.mercadopago.com https://*.mercadolibre.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "media-src 'self'",
+            ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
